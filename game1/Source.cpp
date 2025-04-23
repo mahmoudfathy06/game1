dc07f427ee74145ca4ebca64539ac8fbf69031bc
mahmo <mahmo@DESKTOP-4400O9F>
2025-04-23 14:38:46 +0200

    Add project files.

---
+++ game1/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int x[10][10];
+
+	for (int r = 0; r < 10; r++)
+	{
+		for (int c = 0; c < 10; c++)
+		{
+			cin >> x[r][c];
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
